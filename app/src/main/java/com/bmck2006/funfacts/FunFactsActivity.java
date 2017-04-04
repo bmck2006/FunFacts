f94060e17dd5d998a23eb51e1b0bce2cac2b5170
Brian McKeown <bmckeown2011@gmail.com>
2017-04-04 13:04:28 -0400

    Removed non-essential imports

--- app/src/main/java/com/bmck2006/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/bmck2006/funfacts/FunFactsActivity.java
@@ -1,14 +1,11 @@
 package com.bmck2006.funfacts;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class FunFactsActivity extends AppCompatActivity {
